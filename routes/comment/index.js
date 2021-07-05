3272d2ea6a4a22e2aae5023aa333ba707fbaa498
JMaylor <60825814+JMaylor@users.noreply.github.com>
2021-07-05 11:49:22 +0100

    add route to get all comments for a chirp

--- routes/comment/index.js
+++ routes/comment/index.js
@@ -40,7 +40,7 @@ module.exports = async function (fastify, opts) {
       const { body, chirp_id } = req.body;
       const email = req.user["https://chirper.api/email"];
 
-      // create the chirp
+      // create the comment
       const {
         rows: [comment],
       } = await client.query(
@@ -52,6 +52,62 @@ module.exports = async function (fastify, opts) {
 
       client.release();
       reply.status(201).send(comment);
+    },
+  });
+
+  fastify.route({
+    method: "GET",
+    url: "/:chirpId",
+    schema: {
+      tags: ["Comments"],
+      description: "Get all comments for a particular chirp",
+      response: {
+        200: {
+          type: "array",
+          items: {
+            type: "object",
+            required: [
+              "comment_id",
+              "created_at",
+              "body",
+              "user_name",
+              "handle",
+              "picture",
+            ],
+            properties: {
+              comment_id: { type: "number", description: "Unique identifier" },
+              created_at: { type: "number" },
+              body: { type: "string", description: "comment body" },
+              user_name: { type: "string" },
+              handle: { type: "string" },
+              picture: { type: "string" },
+            },
+          },
+        },
+      },
+    },
+    preValidation: fastify.authenticate,
+    handler: async (req, reply) => {
+      const client = await fastify.pg.connect();
+      const { chirpId } = req.params;
+
+      // create the comment
+      const { rows } = await client.query(
+        `SELECT
+          created_at,
+          comment_id,
+          body,
+          user_name,
+          handle,
+          picture
+        FROM chirp_comment
+        JOIN user_account on chirp_comment.author_email = user_account.email
+        WHERE chirp_comment.chirp = $1;`,
+        [chirpId]
+      );
+
+      client.release();
+      reply.status(200).send(rows);
     },
   });
 };
